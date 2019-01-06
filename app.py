12929a2ea7e4e4b5911242377d79899210e85a8c
Loïc Dutrieux <loic.dutrieux@gmail.com>
2019-01-06 19:52:51 +0100

    implemented search endpoint

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python3
 
 from flask import Flask, jsonify, abort, request, make_response, url_for
+from jsonschema import validate
 
 import ministac
 
 app = Flask(__name__)
 
+SEARCH_SCHEMA = {
+  "$schema": "http://json-schema.org/draft-06/schema#",
+  "title": "STAC Collection Specification",
+  "description": "This object represents valid request body for the search endpoint",
+  "type": "object",
+  "required": [
+    "collection"
+  ],
+  "additionalProperties": True,
+  "properties": {
+    "collection": {
+      "title": "Collection name",
+      "type": "string"
+    },
+    "geom": {
+      "title": "Optional geojson geometry",
+      "type": "object"
+    },
+    "startDate": {
+      "title": "Start date",
+      "type": "string"
+    },
+    "endDate": {
+      "title": "End date",
+      "type": "string"
+    },
+    "maxCloudCover": {
+      "title": "Maximum allowed cloud cover",
+      "type": "number"
+    },
+  }
+}
 
 @app.errorhandler(400)
 def not_found(error):
@@ -38,6 +71,20 @@ def get_item(collection, item):
     except Exception as e:
         abort(404)
     return jsonify(item_meta)
+
+
+@app.route('/ministac/api/v0/search', methods = ['GET'])
+def search():
+    content = request.get_json(silent=True)
+    if content is None:
+        abort(400)
+    # Validate request json
+    try:
+        validate(content, SEARCH_SCHEMA)
+    except Exception as e:
+        abort(400)
+    items = ministac.search(**content)
+    return jsonify(items)
 
 
 if __name__ == '__main__':
